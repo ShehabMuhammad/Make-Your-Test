ab3567b26dfe24e94a304b9c93babfd90b95e6ec
ShehabMuhammad <47852641+ShehabMuhammad@users.noreply.github.com>
2021-04-17 19:23:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var qs; var currQ = 0; var cAns = 0;
-var abcd = ['a','b','c','d']; 
 var results = document.querySelector("#res");
+const feedback = document.querySelector('#feedback');
 function makeCourse() 
-{document.querySelector('#form').style.display="none";
+{
+
+document.querySelector('#form').style.display="none";
 var pop = document.querySelector("#pop").style; pop.display = "block"; pop.color="#666"; pop.float="left"; pop.width="6vw"; pop.marginTop="3vw"; }
 var currentCourse ="";
  //**************************Hereo!!
@@ -13,72 +15,92 @@ var currentCourse ="";
 
  var item = localStorage.getItem("questions") ;
 var questions = JSON.parse( item ) ; 
-let i; let len=Object.keys(questions).length; 
+let i; var len=Object.keys(questions).length; 
 
 var crs=Object.keys(questions) ;
 
 /// Here's TEST!!
 function test(){
 document.querySelector("#res").innerHTML=0;
-qs = Object.keys(  questions[currentCourse]  );  console.log("We typed currC : " + currentCourse);
+if(!currentCourse){return;}
+let currCourse =    questions[currentCourse];
+if(!currCourse){return;}
+qs = Object.keys(  currCourse );
 var len = qs.length ;
 document.querySelector("#questions").innerHTML = " This course has a total of : "+ len  + " Questions.  "; 
 if( len < 4) {return;}
 document.querySelector("#total").innerHTML = len;
-var t=document.querySelector('.test');t.style.display="block"; currQ = 0;
-document.querySelector("#sol").innerHTML = qs[0];
+var t=document.querySelector('#test');t.style.display="block"; currQ = 0;
+document.querySelector("#sol").innerHTML = "Q n." + currQ + " - " + qs[0];
  cAns = num = Math.floor(Math.random() * 4  ); var rightAns=questions[currentCourse][qs[0]]   
 document.querySelector( `#a${num}`).innerHTML =  rightAns    ;  
 cAns = num;
-var loksha = [...qs]; loksha.shift();
-console.log(`${qs[0]} ${rightAns}`)
+var filteredQuestions = [...qs]; filteredQuestions.shift();
+
 for(var i=0;i<4;i++)
 { if( i== num ){continue;}
-console.log(`${loksha} len of lok : ${loksha.length} `)
-var Rando = Math.floor(Math.random() * (loksha.length -1) );
-let elem = loksha[Rando];
-loksha = loksha.filter(e=> loksha.indexOf(e)  != Rando  );
-var numQ = `#a${i}`; console.log(numQ);
+
+var Rando = Math.floor(Math.random() * (filteredQuestions.length -1) );
+let elem = filteredQuestions[Rando];
+filteredQuestions = filteredQuestions.filter(e=> filteredQuestions.indexOf(e)  != Rando  );
+var numQ = `#a${i}`; 
   document.querySelector(numQ).innerHTML=  questions[currentCourse][  elem ] ; 
  }
 }
 var inf=document.querySelector("#info");
 ///       ADD THeM ALL !! /////////***********
-for(i=0;i<len;i++)
-{ var mia = document.createElement("LI");
- var turtle=  crs[i]; mia.id = turtle; mia.innerHTML = turtle;  
-var ul= document.getElementById("courses-list"); ul.appendChild(mia);
-mia.onclick= function(){currentCourse = inf.innerHTML = this.innerHTML ; test();}; }
 
-// Above is the bug..
+for(let i=0;i < len; i++)
+{ 
+
+let mia = document.createElement("A");
+ let nameOfTest=  crs[i];  mia.innerHTML = nameOfTest;  
+let div= document.createElement("LI"); div.id=nameOfTest;
+let ul= document.getElementById("courses-list"); 
+mia.onclick= function(){currentCourse = inf.innerHTML = this.innerHTML ; test();}; 
+let remBut = document.createElement("BUTTON");remBut.innerHTML='x';remBut.style.width='auto';remBut.style.height= mia.height; remBut.style.float="right";
+remBut.onmouseover = ()=>{remBut.style.backgroundColor="red";remBut.style.color="white";}
+remBut.onmouseout = ()=>{remBut.style.backgroundColor="white";remBut.style.color="red";}
+remBut.onclick = ()=>{Remove( div.id );}
+div.width = ul.style.width;
+div.appendChild(mia);div.appendChild(remBut);
+ul.appendChild(div); 
+}
+
 //////////////
 function addQuestions() 
-{ document.querySelector('.test').style.display = "none";
+{ 
+if(!currentCourse){return;}
+
+document.querySelector('#test').style.display = "none";
 document.querySelector('#pop').style.display = "none";
     document.querySelector("#form").style.display = "block";}
 
 function add_it() {
-questions[currentCourse][document.querySelector("#Q").value]= document.querySelector('#A').value;
+let Q = document.querySelector("#Q").value;
+let A = document.querySelector('#A').value;
+if(!A || !Q){return;}
+
+let currCourse = questions[currentCourse];
+if(currCourse == null || currCourse == undefined){
+
+quit(); return;
+}
+currCourse[Q]=A ;
 document.querySelector("#Q").value = ""; document.querySelector('#A').value = "";
 document.querySelector("#questions").innerHTML = " This course has a total of : " + Object.keys(questions[currentCourse]).length + " Questions.  ";
 
 }
 
 function quit(){
+
 localStorage["questions"]=JSON.stringify(questions);
-document.querySelector("#form").style.display = "none";}
+let form = document.querySelector("#form");
+form.childNodes.forEach(e=> e.value = "");
+form.style.display = "none";
+}
 
-/* Fucking coro
-function coro()
-{ var info=document.querySelector("#info"); info.innerHTML = course.id ; info.style.width = "100%"  ; 
-    document.querySelector("#main"); 
 
-document.querySelector("#courses-list").appendChild(course);
-var ret = Object.keys(questions[course.id]) ? 
-for(var fuck in  )
-{ let ref = document.createElement("LI"); ref.innerHTML = fuck; document.querySelector("#questions").appendChild(ref); 
-    }
-} */
 
 /*// *************  test *************************** */
 
@@ -87,26 +109,29 @@ for(var fuck in  )
 /////////////////////////////////////////
 function saveCourse() 
 {
-var nom = document.getElementById("course").value;
+let nom = document.getElementById("course").value;
+if(/^\s+$/g.test(nom) || nom === "" ) {alert("This's not a valid name. "); return; } 
 questions[nom] = {} ; 
-var pop = document.querySelector("#pop").style.display = "none";
-var info=document.querySelector("#info");    
-var course = document.createElement("TR"); 
-course.href ="#";
+let pop = document.querySelector("#pop").style.display = "none";
+let info=document.querySelector("#info");    
+let course = document.createElement("A"); 
+let div= document.createElement("LI");
+
 course.innerHTML = nom; 
-course.setAttribute("id", nom);
-course.style.width = "100%";
-course.onclick = ()=>{currentCourse = info.innerHTML = nom; test();};
+div.setAttribute("id", nom);
+//course.style.width = "100%";
+div.onclick = ()=>{currentCourse = info.innerHTML = nom; test();};
  var hov = () =>course.style.backgroundColor ="rgb(100,100,100,0.5)"; course.onmouseover = hov ; 
  var out = () => course.style.backgroundColor = "white"; course.onmouseout = out ;
 course.questions = {} ; 
-    document.querySelector("#main"); 
-var remBut = document.createElement("BUTTON");remBut.innerHTML='X';remBut.style.width='5px';remBut.style.height='5px';remBut.style.float="right";
-remBut.onhover = ()=>{remBut.style.backgroundColor="red";remBut.style.color="white";}
-remBut.onclick = ()=>{Remove(remBut.parentNode.id);}
-course.appendChild(remBut);
-document.querySelector("#courses-list").appendChild(course);
-
+let remBut = document.createElement("BUTTON");remBut.innerHTML='x';remBut.style.color="red";remBut.style.backgroundColor="white";
+remBut.onmouseover = ()=>{remBut.style.backgroundColor="red";remBut.style.color="white";}
+remBut.onmouseout = ()=>{remBut.style.backgroundColor="white";remBut.style.color="red";}
+remBut.onclick = ()=>{Remove(nom);}
+ localStorage.setItem("questions", JSON.stringify(questions))
+var List = document.querySelector("#courses-list");div.width = List.width ;
+div.appendChild(course);div.appendChild(remBut);
+List.appendChild(div);
 }
 ///////////////////////////////////
 
@@ -115,14 +140,31 @@ function next()
 
 results = results ?? document.getElementById("res"); 
 qs =Object.keys(questions[currentCourse]);
-if(document.querySelector("#r"+cAns).checked == true ){ var x = parseInt(results.innerHTML); x++; results.innerHTML = x;    }
-if(currQ>=(qs.length-1)){ alert("Exercise is over. Your result is : " + results.innerHTML ); 
- document.getElementsByClassName("test")[0].style.display = "none";  
-return  ;}   /*   Here is where the fun begins         */
+let rightAnswer = document.querySelector("#a"+cAns);
+
+let str = "The right answer is : " + (rightAnswer.innerHTML) + " ."
+if( document.querySelector("#r"+cAns).checked === true ){ var x = parseInt(results.innerHTML); 
+x++; 
+results.innerHTML = x;    
+feedback.style.color= "green";
+feedback.innerHTML = "Correct! " + str;
+}
+else {
+feedback.style.color = "red";
+feedback.innerHTML = "Wrong, " + str;
+}
+if(currQ>=(qs.length-1)){ 
+
+feedback.innerHTML = "Exercise is over. Your result is : " + results.innerHTML ; 
+ document.getElementById("test").style.display = "none";  
+return  ;
+}  
+
+ /*   Here is where the fun begins         */
 currQ+=1;
 
 var sol=document.querySelector('#sol');
-sol.innerHTML=qs[currQ];
+sol.innerHTML="Q n." + currQ + " - " +qs[currQ];
 
 var num = Math.floor(Math.random() * 4  );
 nwAns= questions[currentCourse][qs[currQ]]  ;
@@ -140,16 +182,35 @@ arrAns=arrAns.filter( e=> arrAns.indexOf(e) != rNd   );
 }
 
 
-
 function Remove(ID){
 
+try
+{ 
+if(confirm("Are you sure you want to delete this test? : " + ID + ". ")){
 delete questions[ID];
-localStorage.setItem("questions", JSON.stringify(Qs) );
-return true;
+let Node = document.getElementById(ID);
+Node.parentNode.removeChild(Node); 
+
+if(currentCourse === ID) {currentCourse = "";}
+localStorage.setItem("questions", JSON.stringify(questions) );}
+
+return true;}
+catch (err){
+console.log(' There was a problem with removing the element.');
+console.error(err);
+} 
+
 }
 
 
+function ErasePop(){
+document.querySelector("#form").style.display = "none";
+document.querySelector("#course").value = "";
+document.querySelector("#pop").style.display="none";
 
+
+
+}
 
 
 
